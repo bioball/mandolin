819383b82588cc77b4017c4cc7333ab21a4dfdb2
Dan Chao <dan@play-i.com>
2015-10-14 19:53:01 -0700

    add linting, add build before test in travis.yml

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var plumber = require('gulp-plumber');
 var gutil = require('gulp-util');
+var lint = require('gulp-eslint');
+var watch = require('gulp-watch');
 
 gulp.task('default', function(){
-  return gulp.src("./src/**/*.js")  
+  return gulp.src("./src/**/*.js")
+  .pipe(plumber())
+  .pipe(lint())
+  .pipe(lint.format())
   .pipe(babel())
+  .on('error', function(err){
+    gutil.log(err);
+    this.end()
+  })
   .pipe(gulp.dest("./dist/"));
+});
+
+gulp.task('watch', ['default'], function(){
+  return gulp.watch("./src/**/*.js", function(){ return gulp.start('default') });
 });
